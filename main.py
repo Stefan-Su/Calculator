847b0b3b0df59ffed35217d1f14130c2aec54bfe
Stefan Suter <suter@mail.de>
2021-11-16 12:42:21 +0100

    Basic arithmetic operations (+, -, *, /) implemented. There are no control structures yet. If incorrect data is entered, the error is not handled in a controlled manner.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hallo Welt")
+def user_query(text):
+    query = input(text)
+    return query
+
+
+def calculation(number1, number2, calculation_type):
+    result = eval(number1 + calculation_type + number2)
+    return result
+
+
+print("Hello and welcome to the calculator")
+
+calculation_type = user_query("What kind of calculation do you want to do? (+, -, *, /): ")
+print("Calculation type " + calculation_type)
+number1 = user_query("Please enter the first number: ")
+number2 = user_query("Please enter the second number: ")
+
+print(calculation(number1, number2, calculation_type))
