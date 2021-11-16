f4fcb5a1f0a6f30d096f3ececf322dda3ff472a6
Stefan Suter <suter@mail.de>
2021-11-16 14:09:17 +0100

    Revert "Iteration 2 implemented: Create control structures. If the user makes an unwanted entry, an error message is displayed. So that the program does not crash"
    This reverts commit fd10ddf3

--- main.py
+++ main.py
@@ -8,23 +8,11 @@ def calculation(number1, number2, calculation_type):
     return result
 
 
-def main():
-    calculation_type = user_query("What kind of calculation do you want to do? (+, -, *, /): ")
-    print("Calculation type " + calculation_type)
-    number1 = user_query("Please enter the first number: ")
-    number2 = user_query("Please enter the second number: ")
-    print(calculation(number1, number2, calculation_type))
-
-
 print("Hello and welcome to the calculator")
-main()
 
-count = 1
-while count < 2:
-    abort_decision = input("Do you want to continue calculating [c] or quit the program [q]? (")
-    if abort_decision == "c":
-        main()
-    else:
-        count += 1
+calculation_type = user_query("What kind of calculation do you want to do? (+, -, *, /): ")
+print("Calculation type " + calculation_type)
+number1 = user_query("Please enter the first number: ")
+number2 = user_query("Please enter the second number: ")
 
-print("The calculator is shut down!")
+print(calculation(number1, number2, calculation_type))
