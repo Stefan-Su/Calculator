21db4e4e0ba920a8ebfd86e2da630c1a7b6060be
Stefan Suter <suter@mail.de>
2021-11-16 15:44:23 +0100

    Iteration 3 implemented: Create control structures. If the user makes an unwanted entry, an error message is displayed. So that the program does not crash.

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-def user_query(text):
-    query = input(text)
-    return query
-
-
 def calculation(number1, number2, calculation_type):
     result = eval(number1 + calculation_type + number2)
     return result
 
 
+def main():
+    allowed_calculation_types = "(+, -, *, /)"
+    calculation_type = input("What kind of calculation do you want to do? " + allowed_calculation_types)
+    if calculation_type == "+" or calculation_type == "-" or calculation_type == "*" or calculation_type == "/":
+        print("Calculation type " + calculation_type)
+        number1 = input("Please enter the first number: ")
+        number2 = input("Please enter the second number: ")
+        try:
+            float(number1)
+            float(number2)
+            result = calculation(number1, number2, calculation_type)
+            print(result)
+        except ValueError:
+            print("Error: Only numbers are allowed!")
+    else:
+        print("Error: Only the following types of calculation are allowed: " + allowed_calculation_types)
+
+
 print("Hello and welcome to the calculator")
+main()
 
-calculation_type = user_query("What kind of calculation do you want to do? (+, -, *, /): ")
-print("Calculation type " + calculation_type)
-number1 = user_query("Please enter the first number: ")
-number2 = user_query("Please enter the second number: ")
+count = 1
+while count < 2:
+    abort_decision = input("Do you want to continue calculating [c] or quit the program [q] ")
+    if abort_decision == "c":
+        main()
+    elif abort_decision == "q":
+        count += 1
+    else:
+        print("Error: You only have [c] or [q] as a choice")
 
-print(calculation(number1, number2, calculation_type))
+
+print("The calculator is shutting down!")
